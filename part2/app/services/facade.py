28fbed2d4957129c59cabcb5739ab1bd366d126d
Dsa1r <danamusaad22@gmail.com>
2026-07-15 14:39:41 +0300

    Add place management methods in facade.py
    Implemented methods for place management including creation, retrieval, updating, and fetching all places.

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -12,7 +12,32 @@ class HBnBFacade:
         # Logic will be implemented in later tasks
         pass
 
-    # Placeholder method for fetching a place by ID
+
+    #start of places
+    def create_place(self, place_data):
+        """Create and store a new place."""
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
     def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+        """Retrieve a place by ID."""
+        return self.place_repo.get(place_id)
+    
+    def get_all_places(self):
+        """Retrieve all places."""
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        """Update place details if the place exists."""
+        place = self.place_repo.get(place_id)
+        if not place:
+            return None
+        
+        for key in ['title', 'description', 'price', 'latitude', 'longitude', 'owner_id', 'amenities']:
+            if key in place_data:
+                setattr(place, key, place_data[key])
+        
+        self.place_repo.update(place_id, place_data)
+        return place
+        #end of places
