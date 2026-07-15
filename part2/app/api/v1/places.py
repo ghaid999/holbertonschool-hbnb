3949ad3a365e96ec288915e366cf9d0d9fe9f1c9
Dsa1r <danamusaad22@gmail.com>
2026-07-15 18:33:48 +0300

    Update places.py

--- part2/app/api/v1/places.py
+++ part2/app/api/v1/places.py
@@ -60,7 +60,7 @@ class PlaceList(Resource):
       def get(self):
           """Retrieve a list of all places"""
           places = facade.get_all_places()
-          return {'places': [place.to_dict() for place in places]}, 200
+          return [place.to_dict() for place in places], 200
 
 @api.route('/<place_id>')
 class PlaceResource(Resource):
