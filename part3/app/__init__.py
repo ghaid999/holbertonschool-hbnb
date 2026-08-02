a909db0ed53fba3916a299dd73efb5c3d1572dc5
Dsa1r <danamusaad22@gmail.com>
2026-08-02 17:43:12 +0300

    Import DevelopmentConfig in __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,6 +7,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
+from config import DevelopmentConfig
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
