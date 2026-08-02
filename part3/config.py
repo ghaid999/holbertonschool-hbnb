b4dc70e578c3156e450bcfc04176a22cf353aa29
Dsa1r <danamusaad22@gmail.com>
2026-08-02 18:53:26 +0300

    Enhance configuration with database URI and base directory
    Add BASE_DIR and SQLALCHEMY_DATABASE_URI to config.py

--- part3/config.py
+++ part3/config.py
@@ -1,12 +1,18 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        f"sqlite:///{os.path.join(BASE_DIR, 'hbnb.db')}"
+    )
+    
 config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
