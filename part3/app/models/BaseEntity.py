5e7cb6c4722daabef6a0bc8e5f020bbb1fe049a4
Dsa1r <danamusaad22@gmail.com>
2026-08-02 18:05:27 +0300

    Update BaseEntity.py

--- part3/app/models/BaseEntity.py
+++ part3/app/models/BaseEntity.py
@@ -3,13 +3,35 @@
 BaseEntity class.
 """
 
+from app import db
 import uuid
 from datetime import datetime
+from sqlalchemy.sql import func
 
 
 class BaseEntity:
     """Base class for all entities."""
+    
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=func.now())
+    updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
 
+
+    def save(self):
+        """Update the updated_at timestamp and commit changes to the database"""
+        self.updated_at = func.now()
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()
+        
+
+'''
     def __init__(self):
         """Initialize common attributes."""
         self.id = str(uuid.uuid4())
@@ -29,3 +51,4 @@ class BaseEntity:
                 setattr(self, key, value)
 
         self.save()
+'''
